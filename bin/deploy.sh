55fd46395830d919d078b5aa5888bd5d3c4ecbb7
root <ori@uumpa.com>
2017-08-05 06:17:03 +0000

    minor change to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,5 @@ if [ ! -f docker-compose-deployment-combined.yml ]; then
   echo "please run bin/generate-stack-file.py to generate a stack config file"
   echo "you might want to review/edit this stack file before deploying"
 else
-  bin/build.sh
-  docker stack deploy -c docker-compose-deployment-combined.yml
+  docker stack deploy -c docker-compose-deployment-combined.yml kdp
 fi
-
