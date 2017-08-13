28c5cae835fd9c4b0ea722a82ae28b6fb8429b5a
Ori Hoch <ori@uumpa.com>
2017-08-13 19:54:34 +0300

    add support for setting worker concurrency, with default concurrency of 1 (can set to 0 to disable workers)

--- docker-run.sh
+++ docker-run.sh
@@ -6,9 +6,11 @@ sleep 2
 if [ "${1}" == "" ]; then
     dpp init
     rm -f *.pid
-    python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" -A datapackage_pipelines.app -l INFO beat &
-    python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" --concurrency=1 -A datapackage_pipelines.app -Q datapackage-pipelines-management -l INFO worker &
-    python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" --concurrency=4 -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker &
+    if [ "${DPP_WORKER_CONCURRENCY}" != "0" ]; then
+        python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" -A datapackage_pipelines.app -l INFO beat &
+        python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" --concurrency=1 -A datapackage_pipelines.app -Q datapackage-pipelines-management -l INFO worker &
+        python3 -m celery -b "redis://${DPP_REDIS_HOST}:6379/6" --concurrency=$DPP_WORKER_CONCURRENCY -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker &
+    fi
     dpp serve
 else
     /bin/sh -c "$*"
