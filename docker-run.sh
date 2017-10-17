8cedd00c7f2d405c178bc27a933305b91d2e0f80
Ori Hoch <ori@uumpa.com>
2017-10-17 09:51:31 +0300

    handle shutdown properly for app, set correct cpu values

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 export PIPELINES_BIN_PATH="${PIPELINES_BIN_PATH:-bin}"
 export DPP_WORKER_CONCURRENCY="${DPP_WORKER_CONCURRENCY:-0}"
@@ -7,8 +7,8 @@ export DPP_REDIS_HOST="${DPP_REDIS_HOST:-localhost}"
 # wait for redis
 sleep 2
 
-trap "exit 1" SIGTERM;
-trap "exit 2" SIGINT;
+PIDS=""
+GRACEFUL_SHUTDOWN="1"
 
 if [ "${1}" == "" ]; then
     # run all processes - useful for development / testing
@@ -16,28 +16,72 @@ if [ "${1}" == "" ]; then
     rm -f *.pid
     if [ "${DPP_WORKER_CONCURRENCY}" != "0" ]; then
         "${PIPELINES_BIN_PATH}"/celery_start_all.sh &
+        PIDS+="${!} "
     fi
-    dpp serve
+    dpp serve &
+    PIDS+="${!}"
+
 elif [ "${1}" == "workers" ]; then
     if [ "${DPP_WORKER_CONCURRENCY}" == "0" ]; then
         # this is used for idle worker - which allows to run jobs manually on it using ssh
-        while true; do
-            sleep 86400
-        done
+        echo "idling..."
     else
         # runs workers according to DPP_WORKER_CONCURRENCY
-        "${PIPELINES_BIN_PATH}/celery_run.sh" worker -l INFO -n "workers@%h" "--concurrency=${DPP_WORKER_CONCURRENCY}" -Q datapackage-pipelines
+        GRACEFUL_SHUTDOWN="0"
+        "${PIPELINES_BIN_PATH}/celery_run.sh" worker -l INFO -n "workers@%h" "--concurrency=${DPP_WORKER_CONCURRENCY}" -Q datapackage-pipelines &
+        PIDS+="${!}"
     fi
+
 elif [ "${1}" == "management" ]; then
     # runs management processes - must only run once
     dpp init
     "${PIPELINES_BIN_PATH}/celery_run.sh" beat -l INFO &
-    "${PIPELINES_BIN_PATH}/celery_run.sh" worker -l INFO -n "management@%h" --concurrency=1 -Q datapackage-pipelines-management
+    PIDS+="${!} "
+    "${PIPELINES_BIN_PATH}/celery_run.sh" worker -l INFO -n "management@%h" --concurrency=1 -Q datapackage-pipelines-management &
+    PIDS+="${!}"
+
 elif [ "${1}" == "serve" ]; then
     # runs the pipelines dashboard only - can have multiple instances
-    dpp serve
+    dpp serve &
+    PIDS+="${!}"
+
 elif [ "${1}" == "flower" ]; then
-    "${PIPELINES_BIN_PATH}/celery_run.sh" flower --url_prefix=flower
+    exec "${PIPELINES_BIN_PATH}/celery_run.sh" flower --url_prefix=flower &
+    PIDS+="${!}"
+
 else
     /bin/sh -c "$*"
+    exit $?
+
 fi
+
+# waits for PIDS and handles shutdown
+
+kill_handler() {
+    if [ "${PIDS}" != "" ]; then
+        echo "non graceful killing (PIDS = ${PIDS})"
+        for PID in $PIDS; do kill -TERM "${PID}"; done
+        sleep 2
+        for PID in $PIDS; do kill -KILL "${PID}"; done
+    fi
+    exit 0
+}
+
+graceful_handler() {
+    if [ "${PIDS}" != "" ]; then
+        echo "graceful shutdown (PIDS = ${PIDS})"
+        for PID in $PIDS; do kill -TERM "${PID}"; done
+        for PID in $PIDS; do wait "${PID}"; done
+    fi
+    exit 0
+}
+
+if [ "${GRACEFUL_SHUTDOWN}" == "1" ]; then
+    trap "echo 'caught SIGTERM, attempting graceful shutdown'; graceful_handler" SIGTERM;
+    trap "echo 'caught SIGINT, attempting graceful shutdown'; graceful_handler" SIGINT;
+else
+    trap "echo 'caught SIGTERM, performing non-graceful shutdown'; kill_handler" SIGTERM;
+    trap "echo 'caught SIGINT, performing non-graceful shutdown'; kill_handler" SIGINT;
+fi
+
+while true; do tail -f /dev/null & wait ${!}; done
