9122ad6beedd8e95b464aaa47514abd29d955189
Aryeh <aryehpro@gmail.com>
2017-10-21 21:00:06 +0300

    Add initial work on plenum meeting parsing

---
+++ datapackage_pipelines_knesset/plenum/processors/download_plenum_meeting_protocols.py
@@ -0,0 +1,91 @@
+from datapackage_pipelines_knesset.common.processors.base_processor import BaseProcessor
+from requests.exceptions import RequestException
+import os, requests, logging, time, json
+from datapackage_pipelines_knesset.common import object_storage
+
+
+class DownloadPlenumMeetingProtocolsProcessor(BaseProcessor):
+    def __init__(self, *args, **kwargs):
+        super(DownloadPlenumMeetingProtocolsProcessor, self).__init__(*args, **kwargs)
+        self._schema["fields"] = [
+            {
+                "name": "kns_plenum_session_id", "type": "integer",
+                "description": "primary key from kns_plenumsession table"
+            },
+            {
+                "name": "protocol_object_name", "type": "string",
+                "description": "storage object name containing the downloaded protocol"
+            },
+            {
+                "name": "protocol_extension", "type": "string",
+                "description": "file extension of the downloaded protocol"
+            },
+        ]
+        self._schema["primaryKey"] = ["kns_plenum_session_id"]
+        self._all_object_names = []
+
+    def _process(self, datapackage, resources):
+        return self._process_filter(datapackage, resources)
+
+    def _reuqests_get(self, url):
+        return requests.get(url)
+
+    def _save_url(self, url, bucket, object_name, num_retries, seconds_between_retries, retry_num=1):
+        try:
+            res = self._reuqests_get(url)
+        except RequestException as e:
+            if retry_num < num_retries:
+                logging.exception(e)
+                logging.info("retry {} / {}, waiting {} seconds before retrying...".format(retry_num, num_retries,
+                                                                                           seconds_between_retries))
+                time.sleep(seconds_between_retries)
+                return self._save_url(url, bucket, object_name, num_retries, seconds_between_retries, retry_num + 1)
+            else:
+                raise
+        if res.status_code == 200:
+            object_storage.write(bucket, object_name, res.content)
+            return True
+        else:
+            return False
+
+    def _get_extension(self, meeting):
+        ext = meeting["url"].strip()[-4:]
+        if ext in [".doc", ".rtf"]:
+            return ext[1:]
+        elif meeting["url"].strip()[-5:] == ".docx":
+            return "docx"
+        else:
+            logging.warning("unknown extension: {}".format(meeting["url"]))
+
+    def _get_protocol_storage_bucket_name(self):
+        return self._parameters["bucket-name"]
+
+    def _get_protocol_storage_object_name(self, meeting_id, extension):
+        return "{}/{}.{}".format(self._parameters["bucket-name"], meeting_id, extension)
+
+    def _filter_row(self, meeting, **kwargs):
+        bucket = "plenum"
+        protocol_extension = self._get_extension(meeting)
+        object_name = "protocols/original/{}.{}".format(meeting["kns_plenum_session_id"],
+                                                        protocol_extension)
+        override_meeting_ids = os.environ.get("OVERRIDE_COMMITTEE_MEETING_IDS")
+        if not override_meeting_ids or str(meeting["kns_session_id"]) in override_meeting_ids.split(","):
+            if not object_storage.exists(bucket, object_name):
+                override_meeting_ids = os.environ.get("OVERRIDE_COMMITTEE_MEETING_IDS")
+                if not override_meeting_ids or str(meeting["kns_session_id"]) in override_meeting_ids.split(","):
+                    num_retries = self._parameters.get("num-retries", 5)
+                    seconds_between_retries = self._parameters.get("seconds-between-retries", 60)
+                    logging.info("downloading {} -> {}/ {}".format(meeting["url"], bucket, object_name))
+                    if not self._save_url(meeting["url"], bucket, object_name, num_retries, seconds_between_retries):
+                        object_name = None
+            if object_name is not None:
+                # yields all meetings - both ones that were just downloaded, and meetings which were download previously
+                # the only meetings not yielded are meeting which failed downloading
+                yield {"kns_committee_id": meeting["kns_committee_id"],
+                       "kns_session_id": meeting["kns_session_id"],
+                       "protocol_object_name": object_name,
+                       "protocol_extension": protocol_extension}
+
+
+if __name__ == '__main__':
+    DownloadPlenumMeetingProtocolsProcessor.main()
---
+++ datapackage_pipelines_knesset/plenum/processors/load_plenum_meeting_protocols.py
@@ -0,0 +1,39 @@
+from datapackage_pipelines_knesset.common.base_processors.add_resource import AddResourceBaseProcessor
+
+# only loads documents which end with these extensions
+SUPPORTED_APPLICATION_IDS = [1, 4]
+SUPPORTED_GROUP_TYPE_ID = 28
+
+
+class Processor(AddResourceBaseProcessor):
+    def _get_schema(self, resource_descriptor):
+        return resource_descriptor.get("schema", {
+            "fields": [
+                {"name": "url", "type": "string", "description": "url to download protocol from"},
+                {
+                    "name": "kns_plenum_session_id", "type": "integer",
+                    "description": "primary key from kns_plenumsession table"
+                }
+            ],
+            "primaryKey": ["kns_plenum_session_id"]
+        })
+
+    def _get_new_resource(self):
+        plenumsession_table = self.db_meta.tables.get("kns_plenumsession")
+        document_table = self.db_meta.tables.get("kns_documentplenumsession")
+        if plenumsession_table is None or document_table is None:
+            raise Exception("processor requires kns plenum tables to exist")
+        for db_row in self.db_session \
+                .query(plenumsession_table, document_table) \
+                .filter(plenumsession_table.c.PlenumSessionID == document_table.c.CommitteeSessionID) \
+                .filter(document_table.c.ApplicationID.in_(SUPPORTED_APPLICATION_IDS)) \
+                .filter(document_table.c.GroupTypeID == SUPPORTED_GROUP_TYPE_ID) \
+                .all():
+
+            row = db_row._asdict()
+            yield {"url": row["FilePath"],
+                   "kns_plenum_session_id": row["PlenumSessionID"]}
+
+
+if __name__ == "__main__":
+    Processor.main()
