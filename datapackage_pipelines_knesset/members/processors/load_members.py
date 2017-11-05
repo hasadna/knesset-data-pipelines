b2a44da707bd2f53bce68888d73dea9c834fdf77
Michal Nir <michalnir@Michals-Silver.local>
2017-11-05 15:51:20 +0200

    wip

--- datapackage_pipelines_knesset/members/processors/load_members.py
+++ datapackage_pipelines_knesset/members/processors/load_members.py
@@ -1,10 +1,7 @@
 from datapackage_pipelines_knesset.common.base_processors.add_resource import AddResourceBaseProcessor
 from sqlalchemy import or_
+import os
 import logging
-
-
-# only loads documents which end with these extensions
-SUPPORTED_EXTENSIONS = ["doc", "rtf", "docx"]
 
 
 class Processor(AddResourceBaseProcessor):
@@ -13,29 +10,27 @@ class Processor(AddResourceBaseProcessor):
         return resource_descriptor.get("schema", {
             "fields": [
                 {"name": "url", "type": "string", "description": "url to download protocol from"},
-                {"name": "kns_person_id", "type": "integer", "description": "primary key from kns_person table"},
-                {"name": "kns_session_id", "type": "integer", "description": "primary key from kns_committeesession table"}
+                {"name": "kns_person_id", "type": "integer", "description": "primary key from kns_person table"}
             ],
             "primaryKey": ["kns_person_id"]
         })
 
     def _get_new_resource(self):
-        committee_table = self.db_meta.tables.get("kns_committee")
-        committeesession_table = self.db_meta.tables.get("kns_committeesession")
-        documentcommitteesession_table = self.db_meta.tables.get("kns_documentcommitteesession")
-        if committee_table is None or committeesession_table is None or documentcommitteesession_table is None:
+        person_table = self.db_meta.tables.get("kns_person")
+        persontoposition_table = self.db_meta.tables.get("kns_persontoposition")
+        if person_table is None or persontoposition_table is None:
             raise Exception("processor requires kns person tables to exist")
+        override_meeting_ids = os.environ.get("OVERRIDE_PERSON_IDS")
         for db_row in self.db_session\
-            .query(committee_table, committeesession_table, documentcommitteesession_table)\
-            .filter(committeesession_table.c.CommitteeID==committee_table.c.CommitteeID)\
-            .filter(committeesession_table.c.CommitteeSessionID==documentcommitteesession_table.c.CommitteeSessionID)\
+            .query(person_table, persontoposition_table)\
+            .filter(persontoposition_table.c.PersonID==person_table.c.PersonID)\
             .filter(or_(*(documentcommitteesession_table.c.FilePath.like("%.{}".format(e)) for e in SUPPORTED_EXTENSIONS)))\
         .all():
             row = db_row._asdict()
             if str(row["GroupTypeID"]) == "23":
-                yield {"url": row["FilePath"],
-                       "kns_committee_id": row["CommitteeID"],
-                       "kns_session_id": row["CommitteeSessionID"]}
+                if not override_meeting_ids or str(row["PersonID"]) in override_meeting_ids.split(","):
+                    yield {"url": row["FilePath"],
+                        "kns_person_id": row["PersonID"]}
 
 
 if __name__ == "__main__":
