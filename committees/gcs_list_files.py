e71fe0097f9cc4967c8f07ac7877affae8994738
Ori Hoch <ori@uumpa.com>
2018-01-29 09:56:39 +0200

    meetings protocols parser: remove file limit, use gcs files list to only parse new/missing files

---
+++ committees/gcs_list_files.py
@@ -0,0 +1,101 @@
+from datapackage_pipelines.wrapper import ingest, spew
+import logging, os, requests
+from datapackage_pipelines_knesset.retry_get_response_content import get_retry_response_content
+from copy import deepcopy
+from google.cloud import storage
+import regex
+from datapackage_pipelines.utilities.resources import PROP_STREAMING
+
+
+parameters, datapackage, resources = ingest()
+stats = {"total matching files": 0, "invalid files": 0, "protocol parts": 0, "protocol texts": 0, "session documents": 0}
+
+
+protocol_text_regex = "data/committees/meeting_protocols_text/files/([0-9]*)/([0-9]*)/([0-9]*)\.(.*)"
+protocol_parts_regex = "data/committees/meeting_protocols_parts/files/([0-9]*)/([0-9]*)/([0-9]*)\.(.*)"
+session_documents_regex = "data/committees/download_document_committee_session/files/([0-9]*)/([0-9]*)/([0-9]*)/([0-9]*)\.(.*)"
+
+
+storage_client = None
+all_blobs = {}
+if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") or not os.path.exists(os.environ["GOOGLE_APPLICATION_CREDENTIALS"]):
+    raise Exception("integration with google storage is required, environment variable GOOGLE_APPLICATION_CREDENTIALS must point to a google credentials json file")
+
+
+def get_row(blob, file_type, **kwargs):
+    stats["total matching files"] += 1
+    #                1b 10b 50b .5k  1k    5k    10k    20k    50k    100k    1mb      5mb      50mb      100mb      1gb
+    for log_size in (1, 10, 50, 500, 1000, 5000, 10000, 20000, 50000, 100000, 1000000, 5000000, 50000000, 100000000, 1000000000):
+        if blob.size < log_size:
+            stats["files size < {}".format(log_size)] = stats.get("files size < {}".format(log_size), 0) + 1
+            break
+    if file_type == "text":
+        stats["protocol texts"] += 1
+    elif file_type == "parts":
+        stats["protocol parts"] += 1
+    elif file_type == "document":
+        stats["session documents"] += 1
+        stats["docs type {}".format(kwargs["application_desc"])] = stats.get(
+            "docs type {}".format(kwargs["application_desc"]), 0) + 1
+        stats["docs group {}".format(kwargs["group_type_id"])] = stats.get(
+            "docs group {}".format(kwargs["group_type_id"]), 0) + 1
+    else:
+        raise Exception("Invalid file type {}".format(file_type))
+    return {"file_type": file_type,
+           "committee_session_id": kwargs.get("committee_session_id"),
+           "document_committee_session_id": kwargs.get("document_committee_session_id"),
+           "group_type_id": kwargs.get("group_type_id"),
+           "application_desc": kwargs.get("application_desc"),
+           "extension": kwargs.get("extension"),
+           "name": blob.name, "size": blob.size}
+
+
+def get_resource():
+    logging.info("Listing all existing blobs from google storage")
+    storage_client = storage.Client()
+    storage_bucket = storage_client.bucket(os.environ.get("KNESSET_DATA_PIPELINES_BUCKET", "knesset-data-pipelines"))
+    for blob in storage_bucket.list_blobs(prefix="data/committees/"):
+        re_match_text = regex.match(protocol_text_regex, blob.name)
+        re_match_parts = regex.match(protocol_parts_regex, blob.name)
+        re_match_documents = regex.match(session_documents_regex, blob.name)
+        if re_match_text:
+            _, _, committee_session_id, extension = re_match_text.groups()
+            yield get_row(blob, "text",
+                          committee_session_id=int(committee_session_id), extension=extension)
+        elif re_match_parts:
+            _, _, committee_session_id, extension = re_match_parts.groups()
+            yield get_row(blob, "parts",
+                          committee_session_id=int(committee_session_id), extension=extension)
+        elif re_match_documents:
+            group_type_id, _, _, document_committee_session_id, application_desc = re_match_documents.groups()
+            yield get_row(blob, "document",
+                          document_committee_session_id=int(document_committee_session_id),
+                          group_type_id=int(group_type_id), application_desc=application_desc)
+        else:
+            logging.warning("Invalid file: {}".format(blob.name))
+            stats["invalid files"] += 1
+
+
+def get_stats_resource():
+    yield from ({"stat": k, "value": v} for k, v in stats.items())
+
+
+datapackage["resources"] = [{PROP_STREAMING: True,
+                             "name": "files",
+                             "path": "files.csv",
+                             "schema": {"fields": [{"name": "file_type", "type": "string"},
+                                                   {"name": "committee_session_id", "type": "integer"},
+                                                   {"name": "document_committee_session_id", "type": "integer"},
+                                                   {"name": "group_type_id", "type": "integer"},
+                                                   {"name": "application_desc", "type": "string"},
+                                                   {"name": "extension", "type": "string"},
+                                                   {"name": "name", "type": "string"},
+                                                   {"name": "size", "type": "integer"}]}},
+                            {PROP_STREAMING: True,
+                             "name": "stats",
+                             "path": "stats.csv",
+                             "schema": {"fields": [{"name": "stat", "type": "string"},
+                                                   {"name": "value", "type": "integer"}]}}]
+
+
+spew(datapackage, [get_resource(), get_stats_resource()], stats)
--- committees/parse_meeting_protocols.py
+++ committees/parse_meeting_protocols.py
@@ -8,7 +8,9 @@ from datapackage_pipelines_knesset.common import utils
 parameters, datapackage, resources = ingest()
 stats = {"parsed files": 0}
 check_remote_storage = os.environ.get("CHECK_REMOTE_STORAGE", parameters.get("check-remote-storage"))
-files_limit = int(os.environ.get("FILES_LIMIT", parameters.get("files-limit", "50")))
+files_limit = os.environ.get("FILES_LIMIT")
+if files_limit:
+    files_limit = int(files_limit)
 out_path = parameters["out-path"]
 download_from_path = parameters.get("download-from-path")
 download_from_remote_storage = parameters.get("download-from-remote-storage")
@@ -16,12 +18,43 @@ parse_type = parameters["type"]
 errors = []
 
 
-def file_exists(rel_filename):
-    if check_remote_storage:
-        url = check_remote_storage + rel_filename
-        return requests.head(url).status_code == 200
-    else:
-        return None
+resources = list(resources)
+if len(resources) == 2:
+    files_resource, rows_resource = resources
+else:
+    files_resource, rows_resource = None, resources[0]
+
+
+all_blobs = {}
+if files_resource:
+    rows_schema = datapackage["resources"][1]
+    for file_row in files_resource:
+        all_blobs.setdefault(file_row["committee_session_id"], []).append(file_row)
+else:
+    rows_schema = datapackage["resources"][0]
+
+all_rows = {}
+for row in rows_resource:
+    all_rows.setdefault(int(row["CommitteeSessionID"]), []).append(row)
+
+
+download_rows = []
+verify_rows = []
+for committee_session_id, rows in all_rows.items():
+    blobs = all_blobs.get(committee_session_id)
+    for row in rows:
+        if blobs:
+            has_blob = False
+            for blob_row in blobs:
+                if blob_row["committee_session_id"] == row["CommitteeSessionID"]:
+                    has_blob = True
+                    break
+            if has_blob:
+                verify_rows.append(row)
+            else:
+                download_rows.append(row)
+        else:
+            download_rows.append(row)
 
 
 def parse_protocol(output_filename, protocol):
@@ -44,16 +77,20 @@ def parse_protocol(output_filename, protocol):
         raise NotImplementedError
 
 
-def get_resource(resource):
-    for row in resource:
+def get_resource():
+    for row_num, row in enumerate(download_rows):
+        logging.info("{} / {}".format(row_num, len(download_rows)))
         try:
-            original_filename = row["filename"]
+            original_filename = os.path.join("files", str(row["GroupTypeID"]),
+                                             str(row["DocumentCommitteeSessionID"])[0],
+                                             str(row["DocumentCommitteeSessionID"])[1],
+                                             str(row["DocumentCommitteeSessionID"]) + "." + row["ApplicationDesc"])
             ext = os.path.splitext(original_filename)[1].lower()
             output_filename = "files/{}/{}/{}.{}".format(str(row["CommitteeSessionID"])[0],
                                                          str(row["CommitteeSessionID"])[1],
                                                          str(row["CommitteeSessionID"]),
                                                          "csv" if parse_type == "parts" else "txt")
-            if stats["parsed files"] < files_limit and not file_exists(output_filename):
+            if not files_limit or stats["parsed files"] < files_limit:
                 if download_from_path:
                     download_filename = "../data/committees/download_document_committee_session/" + original_filename
                     if os.path.exists(download_filename):
@@ -72,11 +109,14 @@ def get_resource(resource):
                        parsed_filename=output_filename)
             yield row
         except Exception as e:
-            logging.exception("failed to parse CommitteeSessionID {}".format(row["CommitteeSessionID"]))
-            row.update(error=str(e))
+            # there is a bug in knesset-data-python which prevents getting the error message from the exception
+            # TODO: fix this bug
+            error_message = "failed to parse CommitteeSessionID {}".format(row["CommitteeSessionID"])  # , str(e))
+            logging.exception(error_message)
+            row.update(error=error_message)
             errors.append(row)
-            if len(errors) > 1000:
-                raise Exception("Too many errors!")
+            # if len(errors) > 1000:
+            #     raise Exception("Too many errors!")
 
 
 def get_errors_resource():
@@ -85,12 +125,11 @@ def get_errors_resource():
 
 
 def get_resources():
-    for resource in resources:
-        yield get_resource(resource)
+    yield get_resource()
     yield get_errors_resource()
 
 
-datapackage["resources"].append(deepcopy(datapackage["resources"][0]))
+datapackage["resources"] = [rows_schema, deepcopy(rows_schema)]
 datapackage["resources"][0]["schema"]["fields"] += [{"name": "protocol_extension", "type": "string"},
                                                     {"name": "parsed_filename", "type": "string"}]
 datapackage["resources"][1].update(name="errors", path="errors.csv")
