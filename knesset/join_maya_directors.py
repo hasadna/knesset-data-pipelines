9710e3c426b431517f0de5115646bd496ee71ca6
Ubuntu <ubuntu@ip-10-209-139-40.(none)>
2018-02-24 11:57:12 +0200

    fix for join maya directors

--- knesset/join_maya_directors.py
+++ knesset/join_maya_directors.py
@@ -10,13 +10,23 @@ resources_names = [(resource, descriptor["name"]) for resource, descriptor in zi
 def get_resource():
     for resource, name in resources_names:
         for row in resource:
+            row["id"] = row[""]
+            del row[""]
             row["year"] = name
+            for k,v in row.items():
+              if not v:
+                  row[k] = ""
+              else:
+                  row[k] = str(v)
             yield row
 
 
 datapackage["resources"] = [datapackage["resources"][0]]
-datapackage["resources"][0]["schema"]["fields"].append({"name": "year", "type": "integer"})
+for field in datapackage["resources"][0]["schema"]["fields"]:
+    field["type"] = "string"
+datapackage["resources"][0]["schema"]["fields"].append({"name": "year", "type": "string"})
 datapackage["resources"][0].update(name="maya_directors", path="maya_directors.csv")
+datapackage["resources"][0]["schema"]["fields"][0]["name"] = "id"
 
 
 spew(datapackage, [get_resource()])
