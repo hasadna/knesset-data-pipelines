ef944243d0b957299a1647fd5edd72ec9e3ee8f6
Ori Hoch <ori@uumpa.com>
2018-08-04 20:47:47 +0300

    hot reload optimization

--- bin/docker-hot-reload.sh
+++ bin/docker-hot-reload.sh
@@ -2,10 +2,12 @@
 
 [ "$1" == "" ] && echo bin/docker-hot-reload.sh '<new_code_path>' && exit 1
 
-cp -fR /$1/pipelines/* ./ &&\
-rm -fr /$1 &&\
+cd /$1/pipelines/ &&\
 pipenv install --system --deploy --ignore-pipfile &&\
 pip install -U . &&\
+cp -fR ./* /pipelines/ &&\
+cd /pipelines &&\
+rm -fr /$1 &&\
 kill -HUP 1 &&\
 echo Great Success && exit 0
 
