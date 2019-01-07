023b2aec78e8865bb33d70790dc34c05d851da1c
Ori Hoch <ori@uumpa.com>
2019-01-07 21:20:08 +0200

    disable build of jupyter docker image

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-bash .travis.sh deploy &&\
-docker pull orihoch/knesset-data-pipelines-jupyter:latest &&\
-docker build --cache-from orihoch/knesset-data-pipelines-jupyter:latest \
-             -t orihoch/knesset-data-pipelines-jupyter -f jupyter.Dockerfile . &&\
-docker push orihoch/knesset-data-pipelines-jupyter:latest &&\
-docker tag orihoch/knesset-data-pipelines-jupyter:latest \
-           orihoch/knesset-data-pipelines-jupyter:${TRAVIS_COMMIT} &&\
-docker push orihoch/knesset-data-pipelines-jupyter:${TRAVIS_COMMIT}
+bash .travis.sh deploy
+# &&\
+# docker pull orihoch/knesset-data-pipelines-jupyter:latest &&\
+# docker build --cache-from orihoch/knesset-data-pipelines-jupyter:latest \
+#              -t orihoch/knesset-data-pipelines-jupyter -f jupyter.Dockerfile . &&\
+# docker push orihoch/knesset-data-pipelines-jupyter:latest &&\
+# docker tag orihoch/knesset-data-pipelines-jupyter:latest \
+#            orihoch/knesset-data-pipelines-jupyter:${TRAVIS_COMMIT} &&\
+# docker push orihoch/knesset-data-pipelines-jupyter:${TRAVIS_COMMIT}
