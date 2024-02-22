6ac0d63584b376ab1d97467cb9692087bb98158c
Matti Picus <matti.picus@gmail.com>
2024-02-22 21:26:53 +0200

    data flow starts to work

--- airflow/knesset_data_pipelines/members_eng/members_eng.py
+++ airflow/knesset_data_pipelines/members_eng/members_eng.py
@@ -1,6 +1,7 @@
 import os
-import traceback
 from textwrap import dedent
+import traceback
+import json
 
 import dataflows as DF
 from pyquery import PyQuery as pq
@@ -8,37 +9,46 @@ from pyquery import PyQuery as pq
 from .. import db, config
 from ..get_retry_response_content import get_retry_response_content
 
-URL = "https://main.knesset.gov.il/en/MK/APPS/mk/mk-personal-details"
+def get_members_id():
+    """Return an iterable of all valid mk_individual_id
+    """
+    return range(1, 6000)
 
 def iterate_members():
-    for member_id in range(1, 121):
+    for member_id in get_members_id():
+        URL = f"https://knesset.gov.il/WebSiteApi/knessetapi/MKs/GetMkdetailsHeader?mkId={member_id}&languageKey=en"
+        print(f"getting {URL}")
         try:
             content = get_retry_response_content(
-                f"{URL}/{member_id}", None, None, None, retry_num=1,
+                URL, None, None, None, retry_num=1,
                 num_retries=10, seconds_between_retries=10,
                 skip_not_found_errors=True)
         except Exception:
-            treaceback.print_exc()
-            content = None
-            print(f'failed to get {URL}/{member_id}')
+            traceback.print_exc()
+            print(f'failed to get {URL}')
         else:
-            page = pq(content)
-            # Hmm, the page is rendered by javascript, so the get_retry_response is empty
-            import pdb;pdb.set_trace()
+            data = json.loads(content)
+            name = data.get('Name', '')
+            if not name:
+                continue
+            yield {
+                "NameEng": name,
+                "mk_individual_id": member_id,
+            }
 
 def main():
     table_name = 'member_english_names'
     temp_table_name = f'__temp__{table_name}'
     DF.Flow(
         iterate_members(),
-        DF.update_resource(-1, name='member_english_name', path='member_english_name.csv'),
-        DF.dump_to_path(os.path.join(config.KNESSET_PIPELINES_DATA_PATH, 'members', 'memger_english_names')),
+        DF.update_resource(-1, name='member_english_names', path='member_english_names.csv'),
+        DF.dump_to_path(os.path.join(config.KNESSET_PIPELINES_DATA_PATH, 'members', 'member_english_names')),
         DF.dump_to_sql(
             {temp_table_name: {'resource-name': 'member_english_names'}},
             db.get_db_engine(),
             batch_size=100000,
         ),
-    ).process
+    ).process()
     with db.get_db_engine().connect() as conn:
         with conn.begin():
             conn.execute(dedent(f'''
