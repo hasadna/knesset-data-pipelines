80f65ed925c6ba20a33b173604759dd5d5229253
Adam Kariv <adam.kariv@gmail.com>
2024-09-27 16:43:25 +0300

    keey knesset-data at 2.1.5 for now

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="knesset-data-pipelines",
     version=version,
     packages=find_packages(exclude=["tests", "test.*"]),
-    install_requires=["datapackage-pipelines", "knesset-data==2.1.6", "fuzzywuzzy[speedup]"],
+    install_requires=["datapackage-pipelines", "knesset-data==2.1.5", "fuzzywuzzy[speedup]"],
     extras_require={'develop': ["tox", "pytest"]},
     url='https://github.com/hasadna/knesset-data-pipelines',
     license='MIT',
