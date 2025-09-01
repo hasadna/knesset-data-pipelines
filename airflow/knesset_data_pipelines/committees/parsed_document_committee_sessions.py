a37f7d78549d32ac3103269f94bb9cf06ec13aab
Ori Hoch <ori@uumpa.com>
2025-09-01 20:58:00 +0300

    add retry to protocol parsing

--- airflow/knesset_data_pipelines/committees/parsed_document_committee_sessions.py
+++ airflow/knesset_data_pipelines/committees/parsed_document_committee_sessions.py
@@ -6,7 +6,35 @@ import dataflows as DF
 from .. import db, config
 
 
+def parse_retry(type_, error, session_id, retry_report):
+    session_id = str(session_id)
+    if error:
+        ext = 'txt' if type_ == 'text' else 'csv'
+        hash_file = os.path.join(
+            config.KNESSET_PIPELINES_DATA_PATH,
+            'committees', f'meeting_protocols_{type_}', 'files',
+            session_id[0], session_id[1], f'{session_id}.{ext}.hash'
+        )
+        if os.path.exists(hash_file):
+            hash_retry_file = f'{hash_file}.retry'
+            if os.path.exists(hash_retry_file):
+                with open(hash_retry_file) as f:
+                    retry = int(f.read().strip())
+            else:
+                retry = 0
+            retry += 1
+            if retry > 10:
+                print(f'session {session_id} {type_} exceeded max retries')
+                retry_report.append(f'session {session_id} {type_} exceeded max retries')
+            else:
+                with open(hash_retry_file, 'w') as f:
+                    f.write(str(retry))
+                os.remove(hash_file)
+                print(f'session {session_id} {type_} retry {retry}')
+
+
 def process_rows(rows):
+    retry_report = []
     protocol_session_rows = {}
     for row in rows:
         # this if condition should match the one in /committees/filter_document_committee_sessions.py
@@ -16,6 +44,8 @@ def process_rows(rows):
                 and not row["FilePath"].lower().endswith('.docx'))):
             yield row
         else:
+            parse_retry('text', row['text_error'], row['CommitteeSessionID'], retry_report)
+            parse_retry('parts', row['parts_error'], row['CommitteeSessionID'], retry_report)
             protocol_session_rows.setdefault(row['CommitteeSessionID'], []).append(row)
     for session_id, rows in protocol_session_rows.items():
         if len(rows) > 1:
