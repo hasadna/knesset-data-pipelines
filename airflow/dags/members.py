639bb15fe7195e93d33aa80dd4ec0a1fb1e1165d
OriHoch <ori@uumpa.com>
2025-10-28 09:26:23 +0200

    add members_eng DAG

---
+++ airflow/dags/members.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+
+from knesset_data_pipelines.members_eng import members_eng
+
+
+dag_kwargs = dict(
+    default_args={
+        'owner': 'airflow',
+    },
+    schedule_interval='10 0 * * *',
+    start_date=days_ago(1),
+    catchup=False,
+)
+
+
+with DAG('members.members_eng', **dag_kwargs) as dag:
+    PythonOperator(
+        python_callable=members_eng.main,
+        task_id='members_eng'
+    )
