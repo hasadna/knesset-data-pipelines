25d59245cc5acb7ec7df36c3966b788edb665281
Ori Hoch <ori@uumpa.com>
2026-03-20 08:40:43 +0200

    remove background material titles dag - the document names are in the source api data now

--- airflow/dags/committees.py
+++ airflow/dags/committees.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 
-from knesset_data_pipelines.committees import background_material_titles
 from knesset_data_pipelines.committees import parsed_document_committee_sessions
 from knesset_data_pipelines.config import AIRFLOW_DEFAULT_EMAILS
 
@@ -15,14 +14,6 @@ dag_kwargs = dict(
     start_date=days_ago(1),
     catchup=False,
 )
-
-
-with DAG('committees.background_material_titles', **dag_kwargs) as dag:
-    PythonOperator(
-        python_callable=background_material_titles.main,
-        task_id='background_material_titles',
-        email=AIRFLOW_DEFAULT_EMAILS,
-    )
 
 
 with DAG('committees.parsed_document_committee_sessions', **dag_kwargs) as dag:
--- airflow/knesset_data_pipelines/committees/cli.py
+++ airflow/knesset_data_pipelines/committees/cli.py
@@ -7,12 +7,6 @@ def committees():
 
 
 @committees.command()
-def background_material_titles(**kwargs):
-    from .background_material_titles import main
-    main(**kwargs)
-
-
-@committees.command()
 def parsed_document_committee_sessions(**kwargs):
     from .parsed_document_committee_sessions import main
     main(**kwargs)
